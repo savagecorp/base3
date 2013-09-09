a6b0f064a507d1c9eb993ecb53419a0e964687e3
Brett Mascavage <brett@eastmedia.com>
2013-09-09 16:51:31 -0400

    base rails 3 template to mess around with

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Base3::Application.config.secret_token = '493495bf48733ca7bf7c4cfdc963bfc545224c7a7b1dc441e6392fe35051340cf195eec0fc88c914e4c70694432d3d9678fe7a126fa7a405983c543c5b918d45'
