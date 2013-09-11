485657c6e244f4f8abc3f9a2e8c3a61cd6427f51
Brett Mascavage <brett@eastmedia.com>
2013-09-11 17:21:25 -0400

    sprite animation test

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
   
-  $( "#demo3b" ).click(function() {
-    $('#demo3').animo({animation: ['wobble', 'fadeOutUp'] });
-  });
-  
-  $( "#demo4b" ).click(function() {
-    $('#demo4').animo({ animation: ['bounceInDown']}, function(){
-      $('#demo5').animo({ animation: ['lightSpeedOut']} );
-    });
-  });
-  
-  $( "#demo5b" ).click(function() {
-    $('#demo5').animo({animation: ['bounceOut'] });
-  });
-  
-  $( "#demo6b" ).click(function() {
-    $('#demo6').animo({animation: ['flipOutX'] });
-  });
-  
+  $('#sprite').spriteOnHover({fps:12}); 
+  $('#graph').spriteOnHover({fps:16}); 
+
 });
